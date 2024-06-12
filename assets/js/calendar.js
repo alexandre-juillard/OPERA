98eb5b1899c8dd32c441aeeb99e497435cc51589
alexandre-juillard <ajuillard69@gmail.com>
2024-06-12 17:55:35 +0200

    feat(calendar): style in js file

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,16 +1,18 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'; // pour les interactions comme le clic
 
 document.addEventListener('DOMContentLoaded', function () {
     const calendarEl = document.getElementById('calendar');
     if (calendarEl) {
         const calendar = new Calendar(calendarEl, {
-            plugins: [dayGridPlugin, interactionPlugin],
-            initialView: 'dayGridMonth',
+            plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+            initialView: 'timeGridWeek',
             locale: 'fr',
+            timeZone: 'Europe/Paris',
             headerToolbar: {
-                start: 'dayGridMonth,dayGridWeek,dayGridDay',
+                start: 'dayGridMonth,timeGridWeek,timeGridDay',
                 center: 'title',
                 end: 'prev,next'
             },
@@ -22,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
             weekNumbers: true,
             weekNumberFormat: { week: 'numeric' },
             firstDay: 1,
+            nowIndicator: true,
             events: [
                 { title: 'Réunion', start: '2024-04-25' },
                 { title: 'Entretien annuel', start: '2024-04-30' }
@@ -32,3 +35,8 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error('Calendar element not found in the DOM');
     }
 });
+
+//dayGrid = affiche pur
+//timeGrid = affichage avec les heures
+//timeGridDay / timeGridMonth / timeGridWeek = affichage jour, mois, semaine
+//on peut lui passer autant de paramètres que l'on veut pour influer sur affichage des infos
